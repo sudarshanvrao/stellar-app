10d68d928af754a2b4420c684af14d9e7601fb5f
Sudarshan <sudu@Sarigas-MacBook-Pro.local>
2022-03-24 20:17:20 +0530

    UI changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ import {Link} from "react-router-dom";
 export default function Home(){
     return (
         <div className="home">
-            <Link className="home-link" to="/nasaphoto">ASTRONOMY PICTURE OF THE DAY</Link>
-            <Link className="home-link" to="/epic">NASA'S EPIC IMAGE</Link>
+            <Link className="home-link" to="/nasaphoto">NASA - APOD</Link>
+            <Link className="home-link" to="/epic">NASA - EPIC IMAGE</Link>
         </div>
     );
 }
--- src/components/NasaEPIC.js
+++ src/components/NasaEPIC.js
@@ -6,7 +6,7 @@ export default function NasaEPIC(){
     var d = new Date();
     d.setDate(d.getDate() - 2);
     var day = String(d.getDate()).padStart(2, '0');
-    var month = String(d.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var month = String(d.getMonth() + 1).padStart(2, '0');
     var year = String(d.getFullYear());
     
     const [epicMetadata, setEpicMetadata] = useState(null);
@@ -29,7 +29,7 @@ export default function NasaEPIC(){
         <img className="photo" src={`https://api.nasa.gov/EPIC/archive/natural/${year}/${month}/${day}/png/${epicMetadata[0].image}.png?api_key=${apiKey}`} alt="title"/>
         <div>
                 <h1>{"Imagery of the Earth collected by DSCOVR's Earth Polychromatic Imaging Camera (EPIC) instrument"}</h1>
-                <p className="date">{year + '-' + month + '-' + day}</p>
+                <p className="date">{`Capture Date : ${year}-${month}-${day}`}</p>
                 <p className="explanation">{"EPIC (Earth Polychromatic Imaging Camera) is a 10-channel spectroradiometer (317 – 780 nm) onboard NOAA’s DSCOVR (Deep Space Climate Observatory) spacecraft. EPIC provides 10 narrow band spectral images of the entire sunlit face of Earth using a 2048x2048 pixel CCD (Charge Coupled Device) detector coupled to a 30-cm aperture Cassegrain telescope (Figure 1).The DSCOVR spacecraft is located at the Earth-Sun Lagrange-1 (L-1) point giving EPIC a unique angular perspective that will be used in science applications to measure ozone, aerosols, cloud reflectivity, cloud height, vegetation properties, and UV radiation estimates at Earth's surface."}</p>
             </div>
         </div>
